67dac738fe0e3b4a91cc51f1818b1b76d457ef59
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-04-03 14:41:47 +0300

    chore: add seed for roles

---
+++ src/Infrastructure/Persistence/AppDbContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevToDev.Domain.Entities.Identity;
+
+namespace DevToDev.Infrastructure.Persistence
+{
+    public static class AppDbContextSeed
+    {
+        public static async Task SeedRolesAsync(AppDbContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                await context.Roles.AddRangeAsync(new List<Role>
+                {
+                    new()
+                    {
+                        Name = "Admin"
+                    },
+                    new()
+                    {
+                        Name = "User"
+                    }
+                });
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- src/WebUi/Program.cs
+++ src/WebUi/Program.cs
@@ -1,13 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using DevToDev.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 
 namespace WebUi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
@@ -17,7 +23,34 @@ namespace WebUi
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
 
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+
+                    if (context.Database.IsSqlServer())
+                    {
+                        await context.Database.MigrateAsync();
+                    }
+
+                    await AppDbContextSeed.SeedRolesAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+
+                    throw;
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
