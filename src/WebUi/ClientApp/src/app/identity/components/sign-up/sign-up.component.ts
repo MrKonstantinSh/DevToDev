baf6eaba6a1425cbabe8432543a8602765d98579
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-05-03 18:05:01 +0300

    feat(fe): add sign up form validation

--- src/WebUi/ClientApp/src/app/identity/components/sign-up/sign-up.component.ts
+++ src/WebUi/ClientApp/src/app/identity/components/sign-up/sign-up.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { SignUpDto } from "../../dtos/signUpDto";
+import { IdentityService } from "../../services/identity.service";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.component.html",
+  styleUrls: ["./sign-up.component.css"],
 })
 export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private identityService: IdentityService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.createSignUpForm();
   }
 
+  onSubmit() {
+    const signUpDto = new SignUpDto();
+    signUpDto.email = this.signUpForm.value.email;
+    signUpDto.password = this.signUpForm.value.password;
+    signUpDto.rePassword = this.signUpForm.value.rePassword;
+
+    this.identityService.signUp(signUpDto).subscribe(
+      () => {
+        this.router.navigateByUrl("/sign-in");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private createSignUpForm() {
+    this.signUpForm = new FormGroup({
+      email: new FormControl("", [
+        Validators.required,
+        Validators.pattern("[a-z0-9._%-]+@[a-z0-9._%-]+\\.[a-z]{2,4}"),
+      ]),
+      password: new FormControl("", Validators.required),
+      rePassword: new FormControl("", Validators.required),
+    });
+  }
 }
