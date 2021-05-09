fd1e4eac1a036361ae96fc9a8b0c0e0ef3d776be
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-05-09 18:36:01 +0300

    feat(fe): create a page for creating articles

--- src/WebUi/ClientApp/src/app/blog/blog-routing.module.ts
+++ src/WebUi/ClientApp/src/app/blog/blog-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "../shared/guards/auth.guard";
+import { ArticleConstructorComponent } from "./components/article-constructor/article-constructor.component";
 import { FeedComponent } from "./components/search/search.component";
 
 const routes: Routes = [
   {
     path: "search",
     component: FeedComponent,
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "article-constructor",
+    component: ArticleConstructorComponent,
     canLoad: [AuthGuard],
     canActivate: [AuthGuard],
   },
